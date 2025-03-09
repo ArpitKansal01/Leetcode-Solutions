0e708e418f63c43b1789cb56925292a17b6e0119
Arpit Kansal <155746706+ArpitKansal01@users.noreply.github.com>
2025-03-09 19:53:32 +0530

    Time: 24 ms (40.04%), Space: 13 MB (50.31%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        a=''.join(sorted(s))
+        b=''.join(sorted(t))
+        if a==b:
+            return True
+        else:
+            return False
