1e6f71a08c92af8e29e28dff2efa64b8d63de4bb
Arpit Kansal <155746706+ArpitKansal01@users.noreply.github.com>
2025-08-11 17:18:04 +0530

    Time: 0 ms (100%), Space: 8.5 MB (50.62%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if(x==0)    return 0; 
-        for(int i=1;i<=x/2+1;i++){
-            if(i>INT_MAX/i)    return i-1;
-            else if(i*i==x) return i;
-            else if(i*i>x) return i-1;
+        if(x==0)return 0;
+        int left=1,right=x/2;
+        int sqrt;
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            if(mid==x/mid){
+                return mid;
+            }
+            else if(mid<x/mid){
+                sqrt = mid;
+                left = mid+1;
+            }
+            else right = mid-1;
         }
-        return 0;
+        return sqrt;
     }
 };
